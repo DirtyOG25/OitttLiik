4c7d4cf7d7724824c5babbfa651abab43ea28c79
DirtyOG25 <mr420weedz@gmail.com>
2025-07-10 22:14:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-const BOT_TOKEN = "7567650399:AAE-UKQun_sCAITgH11i7rXmH1PykQZbaj8";
-const CHAT_ID = "-4983493143";
+const BOT_TOKEN = "7861659325:AAEZ8m6xiHlpYLGmGWRbsG3G5qw8iY9tlLY";
+const CHAT_ID = "7713690054";
